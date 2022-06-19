583e850ae482f903fac98748e9ee64f8d980f6dd
Zick Keen <jackkinny@gmail.com>
2022-06-19 21:14:03 +0700

    feat: Contact
    Feature Contact

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React from 'react';
 import './App.css';
 
+import Header from '../src/components/Header';
+import Contact from '../src/components/Contact';
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
       
+      <Header />
+      {contacts.map((contact, i) => {
+        return <Contact key={i} data={contact} />
+      })}
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,18 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = (props) => {
     return (
-        <></>
+        <div className="contact">
+            <img src={props.data.photo} className="img-contact" alt="contact" />
+            <div className="contact-info">
+                <h2>{props.data.name}</h2>
+                <p>{props.data.phone}</p>
+                <p>{props.data.email}</p>
+            </div>
+        </div>
     )
 }
 
